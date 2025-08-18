018588495a3cc4349d64979eb135a6f6f4a3bbc4
Trofymenko Vladyslav <naughtysora@proton.me>
2025-08-18 16:42:29 +0300

    tests, symbol dispose

--- test/index.js
+++ test/index.js
@@ -1,10 +1,9 @@
 "use strict";
 
+const tests = ['mutex', 'binary-semaphore', 'semaphore'];
+const skip = process.argv.slice(2);
 
-const mutex = require("./mutex.js");
-const semaphore = require("./semaphore.js");
-const semaphoreFs = require("./semaphore-fs.js");
-
-// mutex();
-semaphore();
-// semaphoreFs();
+for (const test of tests) {
+  if (skip.includes(test)) continue;
+  require(`./${test}.js`);
+}
